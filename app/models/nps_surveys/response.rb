398faf3a8d5e867f58eae184eab8a4638280bede
Jose Fernandez (magec) <joseferper@gmail.com>
2016-07-28 13:24:21 +0200

    Use convention for table name

--- app/models/nps_surveys/response.rb
+++ app/models/nps_surveys/response.rb
@@ -1,7 +1,5 @@
 module NPSSurveys
   class Response < ActiveRecord::Base
-    self.table_name = 'surveys_responses'
-
     validates :survey, presence: true
     validates :user_id, presence: true
   end
