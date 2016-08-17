a653a4e7de452e54176c969e1b094a02753503ad
Jose Fernandez (magec) <joseferper@gmail.com>
2016-08-17 12:37:07 +0200

    Solve inflection issue with NPS acronym

--- config/initializers/defaults.rb
+++ config/initializers/defaults.rb
@@ -3,3 +3,7 @@ NPSSurveys.route_namespace = :api_internal
 NPSSurveys.route_path = 'api/internal'
 NPSSurveys.route_scope = 'nps_surveys'
 NPSSurveys.current_user = lambda { |controller| controller.send(:current_user) }
+
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.acronym 'NPS'
+end
